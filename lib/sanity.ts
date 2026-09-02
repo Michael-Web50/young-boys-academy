959c9c6c631fc0d69362a64343e0782aa5eb997d
0x-Saturn <chideramichael351@gmail.com>
2026-09-02 12:35:39 +0100

    Fix Sanity projectId environment variable name

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,14 +1,14 @@
 import { createClient } from '@sanity/client'
-import imageUrlBuilder, { createImageUrlBuilder } from '@sanity/image-url'
+import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.sd70wpwl,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
   useCdn: process.env.NODE_ENV === 'production',
 })
 
-const builder = createImageUrlBuilder(client)
+const builder = imageUrlBuilder(client)
 
 export function urlFor(source: any) {
   return builder.image(source)
